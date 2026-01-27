cf4d3e4920d24d8b8e9adf69a552a8c622e82264
Олійник Гліб <glib070707@gmail.com>
2026-01-27 16:05:01 +0200

    Add user pydantic schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    username: str = Field(min_length=3, max_length=25)
+    password: str = Field(min_length=8)
+
+
+class UserResponse(UserBase):
+    id: int
+    username: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserLogin(UserBase):
+    password: str = Field(min_length=8)
