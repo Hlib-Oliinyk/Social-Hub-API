4d01b666ae51b6b41f3219c281c35ce907c58570
Олійник Гліб <glib070707@gmail.com>
2026-01-27 13:28:47 +0200

    Create JWT tokens and Token models

--- app/core/security.py
+++ app/core/security.py
@@ -1,11 +1,26 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+from app.core.config import ALGORITHM, SECRET_KEY
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(shemes=["bcrypt"], deprecated="auto")
 
-
-def hash_password(password: str):
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def verify_password(password: str, hashed: str):
+def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
+
+
+def create_access_token(data: dict, expired_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expired_delta:
+        expire = datetime.now(timezone.utc) + expired_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
