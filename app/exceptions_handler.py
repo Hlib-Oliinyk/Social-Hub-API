8cafc3702a982c5b707fe70b52aa9624867e1271
Олійник Гліб <glib070707@gmail.com>
2026-01-28 10:44:33 +0200

    Add global exception handler

---
+++ app/exceptions_handler.py
@@ -0,0 +1,27 @@
+from fastapi.responses import JSONResponse
+from app.exceptions import *
+
+
+def register_exception_handler(app):
+    @app.exception_handler(UserNotFound)
+    async def user_not_found_handler(request, exc):
+        return JSONResponse(
+            status_code=404,
+            content={"detail":"User not found"}
+        )
+
+    @app.exception_handler(UserAlreadyExists)
+    async def user_exists_handler(request, exc):
+        return JSONResponse(
+            status_code=400,
+            content={"detail":"User exists"}
+        )
+
+    @app.exception_handler(InvalidCredentials)
+    async def invalid_credentials_handler(request, exc):
+        return JSONResponse(
+            status_code=401,
+            content={"detail":"Could not validate credentials"},
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
