fc7268da08c40ca98e929c90dac166f5923949ef
Олійник Гліб <glib070707@gmail.com>
2026-01-31 12:54:09 +0200

    Create like schema

---
+++ app/schemas/like.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+
+
+class LikeCreate(BaseModel):
+    user_id: int
+    post_id: int
+
+
+class LikeResponse(BaseModel):
+    user_id: int
+    create_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
